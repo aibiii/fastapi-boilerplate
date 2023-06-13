8691179c774d08eaa0b607018d54d9dd7a63e3ef
Aibibi Kuan <aibikuan@MacBook-Air-aibi.local>
2023-06-13 23:16:02 +0600

    commited changes

--- app/shanyraks/repository/repository.py
+++ app/shanyraks/repository/repository.py
@@ -1,6 +1,8 @@
 from typing import Any
 from bson.objectid import ObjectId
 from pymongo.database import Database
+from pymongo.results import UpdateResult
+
 
 
 class ShanyrakRepository:
@@ -15,7 +17,7 @@ class ShanyrakRepository:
     def get_shanyrak(self, shanyrak_id: str):
         return self.database["shanyraks"].find_one({"_id": ObjectId(shanyrak_id)})
     
-    def update_shanyrak(self, shanyrak_id: str, user_id: str, data: dict[str, Any]):
+    def update_shanyrak(self, shanyrak_id: str, user_id: str, data: dict[str, Any]) -> UpdateResult:
         self.database["shanyraks"].update_one(
             filter={"_id": ObjectId(shanyrak_id), "user_id": ObjectId(user_id)},
             update={
